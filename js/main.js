1b33b0cb7d1da580e8b2013163e8e71a9c66f144
oblik <oblik35000@gmail.com>
2014-08-16 01:25:26 +0200

    1st release

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+    $("#main-container").calendar({
+        tipsy_gravity: 's', // How do you want to anchor the tipsy notification? (n / s / e / w)
+        click_callback: calendar_callback, // Callback to return the clicked date
+        //year: "2012", // Optional, defaults to current year - pass in a year - Integer or String
+        scroll_to_date: false // Scroll to the current date?
+    });
+});
+
+//
+// Example of callback - do something with the returned date
+var calendar_callback = function(date) {
+    console.log(date);
+
+}
