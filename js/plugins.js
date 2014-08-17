ade65fcc33719874d17d5dea4583d92fe9162dea
oblik <oblik35000@gmail.com>
2014-08-17 13:30:13 +0200

    Correct CSS last element

--- js/plugins.js
+++ js/plugins.js
@@ -190,8 +190,8 @@
         $("#owl-carousel-calendar").owlCarousel({
             items : 31, //10 items above 1000px browser width
             itemsDesktop : [1000,31], //5 items between 1000px and 901px
-            itemsDesktopSmall : [900,15], // betweem 900px and 601px
-            itemsTablet: [600,15], //2 items between 600 and 0
+            itemsDesktopSmall : [900,16], // betweem 900px and 601px
+            itemsTablet: [600,6], //2 items between 600 and 0
             itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
         });
 
